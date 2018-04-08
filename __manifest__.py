3ec71dc46bb824c3cee949a7bb4a6f05e953af2e
macbookpro <mouad.hamri@bhecoservices.com>
2018-04-08 10:29:52 +0100

    [Add] project idea model

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'CRM extra fields',
+    'version': '1.0.0',
+    'category': 'PMO Idea',
+    'description': """
+Constitution du portefeuille des projets
+========================
+    """,
+    'depends': ['base','project'],
+    'data': [
+        'views/project_views.xml',
+    ],
+    'installable': True,
+}
